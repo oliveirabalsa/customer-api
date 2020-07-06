ba8dead89fc7969991991820bee9abd33f3f9378
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-07-05 21:43:50 -0300

    refactor: adjustments on docker-compose

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      // database: 'luiza',
-      host: 'host.docker.internal',
+      host: 'db',
       user: 'postgres',
       password: 'postgres'
     },
